52ff4ba2ea1baa301e302f15419526269e5965cb
Mikalai.Lapko <Mikalai_Lapko@epam.com>
2023-09-26 03:56:03 +0200

    Cleanup, reverse post order

--- bot.py
+++ bot.py
@@ -5,26 +5,26 @@ import json
 import os
 import logging
 import sys
-from datetime import timedelta, datetime, timezone
+from datetime import datetime, timezone
 
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,  # Set the logging level to INFO
-    format="%(asctime)s - %(levelname)s - %(message)s",  # Set the logging format
-    stream=sys.stdout,  # Set the stream to sys.stdout
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    stream=sys.stdout,  # Logging to stdout
 )
 
-# Get the login and password from environment variables with default values
+# Get login and password from environment variables
 blue_login = os.getenv("BLUE_LOGIN", "user")
 blue_password = os.getenv("BLUE_PASSWORD", "password")
 
-# Base URL of the forum with query parameters for ordering
+# Base URL of the ResetEra gaming forum with query parameters for ordering
 base_url = (
     "https://www.resetera.com/forums/gaming-forum.7/?order=post_date&direction=desc"
 )
 
 # Get the current Unix time and subtract 7200 seconds (2 hours) as the default value
-default_unix_time = int((time.time() - timedelta(hours=2).total_seconds()))
+default_unix_time = int(time.time()) - 7200
 
 # Check for an environment variable named PAGES_TO_LOAD, use 1 as the default value if not provided
 pages_to_load = int(os.getenv("PAGES_TO_LOAD", 1))
@@ -44,23 +44,20 @@ threads_list = []
 
 # Loop through the specified number of pages
 for page in range(1, pages_to_load + 1):
-    # Construct the URL for the current page
     url = f"{base_url}&page={page}"
 
     try:
-        # Send a GET request to the server
         response = requests.get(url)
-        response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
+        response.raise_for_status()
     except requests.RequestException as e:
         logging.error(f"Failed to retrieve page {page}. Error: {e}")
-        continue  # Skip to the next iteration
+        continue
 
-    # Check if request was successful
     if response.status_code == 200:
-        # Parse the HTML content of the page with BeautifulSoup
+        # Parse the HTML content of the page
         soup = BeautifulSoup(response.text, "html.parser")
 
-        # Find the topics on the page (modify the selector as needed)
+        # Find the topics on the page
         topics = soup.select(".structItem-title a")
 
         # Loop over the topics and add them to the list along with the time and link
@@ -71,17 +68,17 @@ for page in range(1, pages_to_load + 1):
             if struct_item:
                 # Check if the item has an element with class 'structItem-status--sticky', if so, skip it
                 if struct_item.select_one(".structItem-status--sticky"):
-                    continue  # Skip this iteration
+                    continue
 
                 # Find the time tag within the 'structItem' div
                 time_tag = struct_item.find("time", class_="u-dt")
 
-                # Extract Unix time directly from the 'data-time' attribute
+                # Extract Unix time from the 'data-time' attribute
                 unix_time = int(time_tag.get("data-time")) if time_tag else None
 
                 # Skip the thread if its Unix time is less than the input Unix time
                 if unix_time and unix_time < input_unix_time:
-                    continue  # Skip this iteration
+                    continue
 
                 # Extract the link to the thread
                 link = topic.get("href")
@@ -89,7 +86,7 @@ for page in range(1, pages_to_load + 1):
                     f"https://www.resetera.com{link}" if link else "Link: Not Found"
                 )
 
-                # Create a dictionary with the thread's details and add it to the list
+                # Create a dict with the thread's details and add it to the list
                 thread_dict = {
                     "title": topic.text.strip(),
                     "time": unix_time,
@@ -110,14 +107,14 @@ try:
 except Exception as e:
     logging.error(f"Failed to write to {timestamp_file}. Error: {e}")
 
-# Convert the list of dictionaries to a JSON string
+# Convert the list of dicts to a JSON string
 try:
     threads_json = json.dumps(threads_list, indent=4)
     logging.info("Successfully converted threads list to JSON")
 except json.JSONDecodeError as e:
     logging.error(f"Failed to convert threads list to JSON. Error: {e}")
 
-# Bluesky
+# Bluesky init
 now = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
 
 try:
@@ -132,8 +129,8 @@ except requests.RequestException as e:
 except json.JSONDecodeError as e:
     logging.error(f"Failed to decode BlueSky session response. Error: {e}")
 
-# Loop through the threads_list and post each thread title as a link to BlueSky
-for thread in threads_list:
+# Loop through the reversed threads_list (old -> new) and post each thread title as a link to BlueSky
+for thread in reversed(threads_list):
     link = thread["link"]
     title = thread["title"]
 
@@ -148,6 +145,7 @@ for thread in threads_list:
     title_tag = soup.find("meta", property="og:title")["content"]
     description_tag = soup.find("meta", property="og:description")["content"]
 
+    # Upload card image
     image_url = soup.find("meta", property="og:image")["content"]
     try:
         resp = requests.get(image_url)
@@ -169,14 +167,14 @@ for thread in threads_list:
     if len(title) > 300:
         title = title[: max_title_length - 3] + "..."  # Truncate and add ellipsis
 
-    post_text = f"{title}"  # Create post text with truncated thread title
-    byte_end = len(post_text.encode("utf-8"))
+    byte_end = len(title.encode("utf-8"))
 
     post = {
         "$type": "app.bsky.feed.post",
-        "text": post_text,
+        "text": title,
         "createdAt": now,
         "langs": ["en-US"],
+        # Card
         "embed": {
             "$type": "app.bsky.embed.external",
             "external": {
@@ -186,6 +184,7 @@ for thread in threads_list:
                 "thumb": thumb,
             },
         },
+        # Link
         "facets": [
             {
                 "index": {"byteStart": 0, "byteEnd": byte_end},
@@ -208,7 +207,7 @@ for thread in threads_list:
         resp.raise_for_status()
     except requests.RequestException as e:
         logging.error(f"Failed to post to BlueSky. Error: {e}")
-        continue  # Skip to the next iteration in case of an error
+        continue
     except json.JSONDecodeError as e:
         logging.error(f"Failed to decode BlueSky post response. Error: {e}")
-        continue  # Skip to the next iteration in case of an error
+        continue
